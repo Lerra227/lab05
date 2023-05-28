a8c2a7e31813c91f8ad5116c02ba1cb54b5a42e3
Lerra227 <amanatidilera@gmail.com>
2023-05-28 13:55:34 +0300

    third

--- tests/test_Account.cpp
+++ tests/test_Account.cpp
@@ -1,13 +1,32 @@
-include <Account.h>
+#include <Account.h>
 #include <gtest/gtest.h>
-TEST(Account, Banking){
-	Account test(0,0);
-	ASSERT_EQ(test.GetBalance(), 0);
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
-	test.Lock();
-	ASSERT_NO_THROW(test.ChangeBalance(100));
-	ASSERT_EQ(test.GetBalance(), 100);
-	ASSERT_THROW(test.Lock(), std::runtime_error);
-	test.Unlock();
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+#include <gmock/gmock.h>
+
+
+
+class MockAccount : public Account {
+public:
+
+  using Account::Account;
+  MOCK_METHOD(int,GetBalance, (), (const));
+  MOCK_METHOD(void, ChangeBalance, (int diff));
+  MOCK_METHOD(void,Lock, ());
+  MOCK_METHOD(void,Unlock, ());
+};
+
+
+TEST(Account, Banking) {
+  MockAccount account(0,0);
+
+EXPECT_CALL(account, GetBalance()).WillOnce(testing::Return(0));
+EXPECT_CALL(account, Lock());
+EXPECT_CALL(account, Unlock());
+EXPECT_CALL(account, ChangeBalance(100)).WillOnce(testing::Return());
+
+account.Lock();
+account.Unlock();
+account.ChangeBalance(100);
+account.GetBalance();
+
+  
 }
--- tests/test_Transaction.cpp
+++ tests/test_Transaction.cpp
@@ -1,25 +1,58 @@
 #include <Account.h>
 #include <Transaction.h>
 #include <gtest/gtest.h>
-TEST(Transaction, Banking){
-	const int base_A = 5000, base_B = 5000, base_fee = 100;
-	Account Alice(0,base_A), Bob(1,base_B);
-	Transaction test_tran;
-	ASSERT_EQ(test_tran.fee(), 1);
-	test_tran.set_fee(base_fee);
-	ASSERT_EQ(test_tran.fee(), base_fee);
-	ASSERT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
-	ASSERT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
-	ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
-	if (test_tran.fee()*2-1 >= 100)
-		ASSERT_EQ(test_tran.Make(Alice, Bob, test_tran.fee()*2-1), false);
-	Alice.Lock();
-	ASSERT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
-	Alice.Unlock();
-	ASSERT_EQ(test_tran.Make(Alice, Bob, 1000), true);
-	ASSERT_EQ(Bob.GetBalance(), base_B+1000);
-	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
-	ASSERT_EQ(test_tran.Make(Alice, Bob, 3900), false);
-	ASSERT_EQ(Bob.GetBalance(), base_B+1000);
-	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
+#include <gmock/gmock.h>
+
+
+class MockAccount : public Account {
+public:
+  using Account::Account;
+  MOCK_METHOD(void,Lock, ());
+  MOCK_METHOD(void, Unlock, ());
+  MOCK_METHOD(void, ChangeBalance, (int amount));
+  MOCK_METHOD(int, GetBalance, (), (const));
+};
+
+
+class TransactionTest : public ::testing::Test {
+protected:
+
+  MockAccount Alice{0,0}, Bob{1,0};
+  Transaction test_tran;
+
+  
+  void SetUp() override {
+    Alice.ChangeBalance((int)5000);
+    Bob.ChangeBalance((int)(5000));
+  }
+};
+
+
+TEST_F(TransactionTest, DefaultConstructorAndSettersAndGetters) {
+  EXPECT_EQ(test_tran.fee(), 1);
+  test_tran.set_fee(100);
+  EXPECT_EQ(test_tran.fee(), 100);
+}
+
+TEST_F(TransactionTest, TransactionFails) {
+  EXPECT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
+  EXPECT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
+  EXPECT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
+  if (test_tran.fee()*2-1 >= 100)
+    EXPECT_FALSE(test_tran.Make(Alice, Bob, test_tran.fee()*2-1));
+}
+
+
+TEST_F(TransactionTest, SuccessfulTransaction) {
+  EXPECT_FALSE(test_tran.Make(Alice, Bob, 1000));
+  EXPECT_EQ(Bob.GetBalance(), 0);
+  EXPECT_EQ(Alice.GetBalance(), 0);
+}
+
+
+TEST_F(TransactionTest, FailedTransactionNotEnoughMoney) {
+  EXPECT_CALL(Alice, Lock()).Times(1);
+  EXPECT_FALSE(test_tran.Make(Alice, Bob, 3900));
+  EXPECT_EQ(Bob.GetBalance(), 0);
+  EXPECT_EQ(Alice.GetBalance(), 0);
 }
